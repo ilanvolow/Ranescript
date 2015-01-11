b33eb6bfd394bd05cac799bd8d88fb95e64a2e88
Ilan Volow <volowig@upmc.edu>
2015-01-10 22:40:54 -0500

    Adding abstract template

---
+++ Abstract.rs
@@ -0,0 +1,25 @@
+
+namespace MyProject
+
+class MyClass inherits Object implements Object
+
+	properties: 
+
+		class:
+		
+		public:
+		
+		protected:
+		
+		private:
+
+	methods:
+
+		class:
+		
+		public:
+		
+		protected:
+		
+		private:
+
